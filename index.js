b94b7b74aa7f85f29b43aed5db4be422a082c855
Shrinidhi Kulkarni <shrinidhi.kulkarni@betsol.com>
2019-03-04 10:26:48 +0530

    Added .env and .env.enc file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 function readDotEnv() {
     console.log('---------> read dot env output goes here <---------');
+    console.log("env wanted to say", process.env.HELLO);
 }
 
 readDotEnv();
